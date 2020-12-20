a1a105c673ce541bb066d26d283d49eaa4d2c5c6
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-12-20 13:06:41 +0100

    Add a very basic scaffolding

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const material = [MatButtonModule];
+const material = [
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule,
+  MatToolbarModule,
+];
 
 @NgModule({
   imports: [material],
