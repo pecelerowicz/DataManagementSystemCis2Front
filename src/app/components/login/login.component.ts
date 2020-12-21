6fad5ccba04c575a99ab62f95934a0ead5b6c785
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-12-21 10:27:57 +0100

    Submit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  login(loginForm: NgForm) {
-    console.log(loginForm.value, loginForm.valid);
+  login(loginForm: NgForm, submit) {
+    console.log(loginForm.value, loginForm.valid, submit);
   }
 }
