91f1883e13cbd1122ec87a6395d0483656c26f01
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-12-29 14:15:04 +0100

    Add snackbar on successful login (to be revisited)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { LoginRequest } from './login-request';
 import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +13,34 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent implements OnInit {
   loginRequest: LoginRequest;
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {
     this.loginRequest = {
       username: '',
       password: '',
     };
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      console.log('a');
+      console.log(params);
+      if (params.registered !== undefined && params.registered === 'true') {
+        console.log('b');
+        this._snackBar.open(
+          'Registration Successful. Check your inbox for activation link.',
+          '',
+          {
+            duration: 6000,
+          }
+        );
+      }
+    });
+  }
 
   login(loginForm: NgForm, submit) {
     // console.log(loginForm.value, loginForm.valid, submit);
@@ -27,10 +49,16 @@ export class LoginComponent implements OnInit {
 
     this.authService.login(this.loginRequest).subscribe(
       () => {
-        console.log('Login successful');
+        //console.log('Login successful');
+        this._snackBar.open('Login Successful.', '', {
+          duration: 6000,
+        });
       },
       (err) => {
-        console.log('error');
+        // console.log('error');
+        this._snackBar.open('Login failed. Please try again.', '', {
+          duration: 6000,
+        });
       }
     );
   }
