3b8d81a9ed0a682e88330d7a8ab6c6702781e1bc
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2021-01-18 10:32:30 +0100

    Display all main folders of the user

--- src/app/components/home/storage-list/storage-list.component.ts
+++ src/app/components/home/storage-list/storage-list.component.ts
@@ -2,7 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import {
   StorageListService,
   Package,
+  Packages,
 } from '../../../services/storage-list.service';
+
+export interface PeriodicElement {
+  name: string;
+  description: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { description: '1', name: 'Hydrogennnnnnnnn4444nsssnnggssnnssssnn' },
+  { description: '2', name: 'Helium' },
+  { description: '3', name: 'Lithium' },
+  { description: '4', name: 'Beryllium' },
+  { description: '5', name: 'Boron' },
+  { description: '6', name: 'Carbon' },
+  { description: '7', name: 'Nitrogen' },
+  { description: '8', name: 'Oxygen' },
+  { description: '9', name: 'Fluorine' },
+  { description: '10', name: 'Neon' },
+];
 
 @Component({
   selector: 'app-storage-list',
@@ -12,21 +31,23 @@ import {
 export class StorageListComponent implements OnInit {
   constructor(private storageListService: StorageListService) {}
 
-  dataSource: Package[] = [];
-
-  ngOnInit() {
-    let a: Package[] = [];
+  ngOnInit(): void {
+    let fetch: PeriodicElement[] = [];
     this.storageListService.getChildren().subscribe((val) => {
-      for (let entry of val.children) {
-        this.dataSource.push(entry);
-        a.push(entry);
+      // console.log(val);
+      //fetch = val;
+
+      let counter: number = 1;
+      for (let a of val.children) {
+        fetch.push({ description: counter.toString(), name: a.relative });
+        counter++;
       }
+      console.log(fetch);
+
+      this.dataSource = fetch;
     });
-    setTimeout(() => {
-      this.dataSource = a;
-    }, 1000);
   }
 
-  displayedColumns: string[] = ['relative'];
-  //dataSource = [{ relative: 'michal' }]; //this.names;
+  displayedColumns: string[] = ['description', 'name', 'info', 'storage'];
+  dataSource = ELEMENT_DATA;
 }
