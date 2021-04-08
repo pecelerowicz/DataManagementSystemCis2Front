e87a8f0bc62cfe20dc7d7bac3165833e5efc4f65
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2021-04-08 13:43:45 +0200

    Fix logging (1)

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  isLoggedIn: boolean;
+  username: string;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.authService.loggedIn.subscribe((data: boolean) => this.isLoggedIn = data);
+    this.authService.username.subscribe((data: string) => this.username = data);
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.username = this.authService.getUserName();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.router.navigateByUrl('');
+  }
+
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterRequest } from '../components/register/register.request';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { LocalStorageService } from 'ngx-webstorage';
 import { LoginRequest } from '../components/login/login-request';
 import { LoginResponse } from '../components/login/login-response';
@@ -13,8 +13,13 @@ import { environmentCustom } from 'src/environments/environment.custom';
 })
 export class AuthService {
 
-  // private address: string = "localhost";
-  //private address: string = "134.122.73.65"
+  @Output() loggedIn: EventEmitter<boolean> = new EventEmitter();
+  @Output() username: EventEmitter<string> = new EventEmitter();
+
+  refreshTokenPayload = {
+    refreshToken: this.getRefreshToken(),
+    username: this.getUserName()
+  }
 
   constructor(
     private httpClient: HttpClient,
@@ -47,7 +52,33 @@ export class AuthService {
       );
   }
 
+  logout() {
+    this.httpClient.post(environmentCustom.address + "/api/auth/logout", this.refreshTokenPayload,
+      { responseType: 'text' })
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        throwError(error);
+      })
+    this.localStorage.clear('authenticationToken');
+    this.localStorage.clear('username');
+    this.localStorage.clear('refreshToken');
+    this.localStorage.clear('expiresAt');
+  }
+
   getJwtToken() {
     return this.localStorage.retrieve('authenticationToken');
+  }
+
+  getUserName(): string {
+    return this.localStorage.retrieve('username');
+  }
+
+  getRefreshToken() {
+    return this.localStorage.retrieve('refreshToken');
+  }
+
+  isLoggedIn(): boolean {
+    return this.getJwtToken() != null;
   }
 }
