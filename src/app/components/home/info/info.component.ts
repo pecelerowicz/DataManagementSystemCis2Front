dd6018ab4eb9097543f6ed78821b55b65904b664
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2021-04-17 16:59:06 +0200

    First steps in reactive forms 2

--- src/app/components/home/info/info.component.ts
+++ src/app/components/home/info/info.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { createPasswordStrengthValidator } from '../../../validators/password-strength.validator';
 
@@ -10,33 +10,65 @@ import { createPasswordStrengthValidator } from '../../../validators/password-st
 })
 export class InfoComponent implements OnInit {
   order: number;
+  disabled: boolean = true;
 
-  name: FormControl = new FormControl(
-    {value: 'Name value taken from the backend', disabled: true}, 
-    {
-      validators: [Validators.required, Validators.minLength(10)],
-      updateOn: 'blur'
-    });
-
-  owner: FormControl = new FormControl(
-    {value: 'Owner value taken from the backend', disabled: true}, 
-    {
-      validators: [Validators.required, Validators.minLength(10)], // to be changed
-      updateOn: 'blur'
-    });
-
-  form: FormGroup = new FormGroup({
-    name: this.name,
-    owner: this.owner
+  form: FormGroup = this.fb.group({
+    name: [
+      {
+        value: 'Name value taken from the backend', 
+        disabled: this.disabled
+      },
+      {
+        validators: [Validators.required, Validators.minLength(10)],
+        updateOn: 'blur'
+      }
+    ],
+    owner: [
+      {
+        value: 'Owner value taken from the backend', 
+        disabled: this.disabled
+      },
+      {
+        validators: [Validators.required, Validators.minLength(8)],
+        updateOn: 'blur'
+      }
+    ]
   });
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     // this.order = parseInt(this.route.snapshot.paramMap.get('order'));
     this.route.paramMap.subscribe((params: ParamMap) => {
       console.log(parseInt(params.get('order')))
       this.order = parseInt(params.get('order'));
+    });
+  }
+
+  toggleDisable() {
+    this.disabled = !this.disabled;
+
+    this.form = this.fb.group({
+      name: [
+        {
+          value: 'Name value taken from the backend', 
+          disabled: this.disabled
+        },
+        {
+          validators: [Validators.required, Validators.minLength(10)],
+          updateOn: 'blur'
+        }
+      ],
+      owner: [
+        {
+          value: 'Owner value taken from the backend', 
+          disabled: this.disabled
+        },
+        {
+          validators: [Validators.required, Validators.minLength(8)],
+          updateOn: 'blur'
+        }
+      ]
     });
   }
 }
