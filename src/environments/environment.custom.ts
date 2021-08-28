2a9f5186ecc9ccc9731250c358c83d2adf8d864c
Michał Pecelerowicz <michal.pecelerowicz@ncbj.gov.pl>
2021-08-28 20:33:20 +0200

    change address

--- src/environments/environment.custom.ts
+++ src/environments/environment.custom.ts
@@ -1,5 +1,4 @@
 export const environmentCustom = {
-    address: 'http://localhost:8080'
-    // address: 'http://134.122.73.65:8080'
-    // address: 'http://207.154.237.47:8080'
+    //address: 'http://localhost:8080'
+    address: 'http://134.122.86.52:8080'    
 }
