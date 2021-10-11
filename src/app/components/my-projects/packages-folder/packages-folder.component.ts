2a4dc0fdd820504bdc7562e4cededaa440747165
Michał Pecelerowicz <michal.pecelerowicz@ncbj.gov.pl>
2021-10-11 08:51:54 +0200

    Downloading files

--- src/app/components/my-projects/packages-folder/packages-folder.component.ts
+++ src/app/components/my-projects/packages-folder/packages-folder.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../../../services/project.service';
 import { SharedCommunicationService } from '../../../services/shared-communication.service';
+import { Node } from '../../../dto/storage';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+import { UploadService } from '../../../services/upload.service';
+
+
+export interface DialogData {
+  order: number;
+  name: string;
+  subfolderName: string;
+}
+
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+  folder: boolean;
+}
 
 @Component({
   selector: 'app-packages-folder',
@@ -9,8 +27,32 @@ import { SharedCommunicationService } from '../../../services/shared-communicati
 })
 export class PackagesFolderComponent implements OnInit {
 
+  private _transformer = (node: Node, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.relative,
+      level: level,
+      folder: node.folder
+    };
+  };
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
   constructor(private sharedCommunicationService: SharedCommunicationService,
-              private projectService: ProjectService) { }
+              private projectService: ProjectService,
+              private uploadService: UploadService) { }
 
   state: {projectId: number, infoName: string, userName: string} = {projectId: -1, infoName: '', userName: ''};
 
@@ -21,7 +63,21 @@ export class PackagesFolderComponent implements OnInit {
       this.state.projectId, this.state.userName, this.state.infoName
     ).subscribe(val => {
       console.log(val);
+      this.dataSource.data = val.children;
+
     })
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
+  isEmptyFolder = (_: number, node: ExampleFlatNode) => node.folder && !node.expandable;
+
+  onDownload(val) {
+    // this.uploadService.download(this.name, val);
+    this.uploadService.downloadFileOfProject(
+      this.state.projectId, this.state.userName, this.state.infoName, val
+    );
+    console.log(val);
   }
 
 }
