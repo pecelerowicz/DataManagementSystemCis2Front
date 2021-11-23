a555958163bebd052f922e908d9469324d152adc
Michał Pecelerowicz <michal.pecelerowicz@ncbj.gov.pl>
2021-11-23 15:09:25 +0100

    Remove 8080

--- src/environments/environment.custom.ts
+++ src/environments/environment.custom.ts
@@ -1,5 +1,5 @@
 export const environmentCustom = {
     //address: 'http://localhost:8080'
     //address: 'http://134.122.86.52:8080'
-    address: 'http://nomaten-db.cis.gov.pl:8080'
+    address: 'http://nomaten-db.cis.gov.pl'
 }
