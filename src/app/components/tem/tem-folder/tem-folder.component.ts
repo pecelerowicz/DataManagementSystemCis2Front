2322ea27b7e558e3418b066fe42c3a5ca4f4af4a
Michal Pecelerowicz <Michal.PecelerowiczM@ncbj.gov.pl>
2023-12-01 12:20:50 +0100

    Displaying folder structure

--- src/app/components/tem/tem-folder/tem-folder.component.ts
+++ src/app/components/tem/tem-folder/tem-folder.component.ts
@@ -3,18 +3,14 @@ import { Component, OnInit } from '@angular/core';
 import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource } from '@angular/material/tree';
 import { Node } from '../../../dto/my_data';
 import { TemFolderService } from 'src/app/services/tem-folder.service';
-
-
-/**
- * Food data with nested structure.
- * Each node has a name and an optional list of children.
- */
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
  
+
+interface TreeNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+  folder: boolean;
+}
 
 @Component({
   selector: 'app-tem-folder',
@@ -24,20 +20,42 @@ interface FoodNode {
 export class TemFolderComponent implements OnInit {
   TREE_DATA: Node[] = [];
 
-  treeControl = new NestedTreeControl<Node>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<Node>();
+  // TREE API
+  treeControl = new FlatTreeControl<TreeNode>((node) => node.level, (node) => node.expandable);
+  
+  private _transformer = (node: Node, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.relative,
+      level: level,
+      folder: node.folder
+    };
+  };
+  
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+  // END OF TREE API
+
+  hasChild = (_: number, node: TreeNode) => node.expandable;
+
+  isEmptyFolder = (_: number, node: TreeNode) => node.folder && !node.expandable;
 
   constructor(private temFolderService: TemFolderService) {
     this.dataSource.data = this.TREE_DATA;
   }
+
   ngOnInit(): void {
       this.temFolderService.getTemMainFolder().subscribe((val) => {
-      this.TREE_DATA = (val.folderStructure).children
+      this.TREE_DATA = val.folderStructure.children
       this.dataSource.data = this.TREE_DATA;
+      console.log(this.TREE_DATA);
     });
-
-
   }
-
-  hasChild = (_: number, node: Node) => !!node.children && node.children.length > 0;
+  
 }
